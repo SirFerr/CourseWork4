30db04030e1cc52775c0e4968c9207317484044b
Ferr <ursusmartimus007@gmail.com>
2023-03-16 11:59:09 +0300

    Added ListAdapter, Apartment and list_item.xml

---
+++ app/src/main/java/com/example/pract__2/Apartment.java
@@ -0,0 +1,31 @@
+package com.example.pract__2;
+
+public class Apartment {
+    private int apartmentID;
+    private int imageID;
+    private String apartmentName;
+
+    public int getApartmentID() {
+        return apartmentID;
+    }
+
+    public void setApartmentID(int apartmentID) {
+        this.apartmentID = apartmentID;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public String getApartmentName() {
+        return apartmentName;
+    }
+
+    public void setApartmentName(String nameOfApartment) {
+        this.apartmentName = nameOfApartment;
+    }
+}
--- app/src/main/java/com/example/pract__2/MainMenuFragment.java
+++ app/src/main/java/com/example/pract__2/MainMenuFragment.java
@@ -1,13 +1,22 @@
 package com.example.pract__2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainMenuFragment extends Fragment {
     View view;
@@ -27,7 +36,30 @@ public class MainMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+        List<Apartment> apartments = new ArrayList<Apartment>();
+        for (int i = 0; i < 200; i++) {
+            Apartment temp = new Apartment();
+            temp.setApartmentID(i);
+            temp.setImageID(R.drawable.cabinetlog);
+            temp.setApartmentName(String.valueOf(i + 1));
+            apartments.add(new Apartment());
+        }
+
+        final ArrayAdapter<Apartment> listAdapter = new ListAdapter(getContext(), R.layout.list_item, apartments);
+        ListView listView = view.findViewById(R.id.ListView);
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
+                                    long id) {
+                Toast.makeText(getContext(), ((TextView) itemClicked).getText(),
+                        Toast.LENGTH_SHORT).show();
+                Log.d("ListView", (String) ((TextView) itemClicked).getText());
+            }
+        });
+
         Bundle bundle = getArguments();
         if (bundle != null) {
             TextView textView = (TextView) view.findViewById(R.id.emailName);
