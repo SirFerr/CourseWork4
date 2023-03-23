e0d1c156d3bc3714dfe0e7d6372d5d59d802a67a
Ferr <ursusmartimus007@gmail.com>
2023-03-23 14:07:26 +0300

    added showNotification

--- app/src/main/java/com/example/pract__2/LogInActivity.java
+++ app/src/main/java/com/example/pract__2/LogInActivity.java
@@ -17,44 +17,4 @@ public class LogInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
     }
-//    public void Change(View view) {
-//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-//        switch (view.getId()) {
-//            case R.id.continueBtn: {
-//                Bundle bundle = new Bundle();
-//                bundle.putString("email", String.valueOf(((EditText) loginFragment
-//                        .getView()
-//                        .findViewById(R.id.editTextTextEmailAddress))
-//                        .getText()));
-//                mainMenuFragment.setArguments(bundle);
-//                ft
-//                        .replace(R.id.fragment_container_view, mainMenuFragment);
-//                break;
-//            }
-//            case R.id.regBtn: {
-//                Bundle bundle = new Bundle();
-//                bundle.putString("email", String.valueOf(((EditText) loginFragment
-//                        .getView()
-//                        .findViewById(R.id.editTextTextEmailAddress))
-//                        .getText()));
-//                linearFragment.setArguments(bundle);
-//                ft
-//                        .replace(R.id.fragment_container_view, linearFragment);
-//                break;
-//            }
-//            case R.id.addApartmentBtn: {
-//                ft
-//                        .replace(R.id.fragment_container_view, relativeFragment);
-//                break;
-//            }
-//            case R.id.regBtnNew: {
-//                Log.d("regBtnNew","click");
-//                linearFragment.clickRegBtnNew();
-//                ft
-//                        .replace(R.id.fragment_container_view,loginFragment);
-//                break;
-//            }
-//        }
-//        ft.addToBackStack(null).commit();
-//    }
 }
--- app/src/main/java/com/example/pract__2/LoginFragment.java
+++ app/src/main/java/com/example/pract__2/LoginFragment.java
@@ -1,5 +1,8 @@
 package com.example.pract__2;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,32 +12,46 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 public class LoginFragment extends Fragment {
-
+    private static final int NOTIFY_ID = 0;
+    private final String CHANNEL_ID = "channel_id";
     View view;
 
-    String debugTAG = "fragmentLifeCycle";
-
-    public LoginFragment() {
-        super(R.layout.fragment_login);
+    private void showNotification(String title, String text) {
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(getContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(getContext());
+        notificationManager.notify(NOTIFY_ID, builder.build());
+        // notificationId - должен быть уникальным для каждого уведомления в канале
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         Toast.makeText(getActivity(), "onCreate", Toast.LENGTH_SHORT).show();
-        Log.d(debugTAG, "onCreate");
-        super.onCreate(savedInstanceState);
-    }
+        Log.d("onCreate", "onCreate");
 
-    @Override
-    public void onStart() {
-        Toast.makeText(getActivity(), "onStart", Toast.LENGTH_SHORT).show();
-        Log.d(debugTAG, "onStart");
-        super.onStart();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Создание канала уведомлений
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Channel name",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Channel description");
+            NotificationManager notificationManager = getActivity().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -43,31 +60,27 @@ public class LoginFragment extends Fragment {
         EditText editText = (EditText) view.findViewById(R.id.editTextTextEmailAddress);
         ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
         editText.setText(getResources().getString(R.string.testEmail));
-        imageView.setImageResource(R.drawable.transparency_demonstration_1);
+        imageView.setImageResource(R.drawable.cabinetlog);
 
         Bundle bundle = getArguments();
         if (bundle != null) {
             ((EditText) view.findViewById(R.id.editTextTextEmailAddress)).setText(bundle.getString("email"));
         }
-        view.findViewById(R.id.continueBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putString("email", String.valueOf(((EditText) view
-                        .findViewById(R.id.editTextTextEmailAddress))
-                        .getText()));
-                Navigation.findNavController(view).navigate(R.id.action_loginFragment2_to_mainMenuFragment,bundle);
-            }
+        view.findViewById(R.id.continueBtn).setOnClickListener(v -> {
+            Bundle bundle12 = new Bundle();
+            bundle12.putString("email", String.valueOf(((EditText) view
+                    .findViewById(R.id.editTextTextEmailAddress))
+                    .getText()));
+            showNotification("Title", "Text");
+
+//                Navigation.findNavController(view).navigate(R.id.action_loginFragment2_to_mainMenuFragment,bundle);
         });
-        view.findViewById(R.id.regBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putString("email", String.valueOf(((EditText) view
-                        .findViewById(R.id.editTextTextEmailAddress))
-                        .getText()));
-                Navigation.findNavController(view).navigate(R.id.action_loginFragment2_to_linearFragment, bundle);
-            }
+        view.findViewById(R.id.regBtn).setOnClickListener(v -> {
+            Bundle bundle1 = new Bundle();
+            bundle1.putString("email", String.valueOf(((EditText) view
+                    .findViewById(R.id.editTextTextEmailAddress))
+                    .getText()));
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment2_to_linearFragment, bundle1);
         });
 
         return view;
